37b42387b2a22fe5f61ea0afc3d398d2e58b6b96
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 19:59:56 +0530

    add new function

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ from art import logo, vs
 
 compare_A = random.choice(data)
 against_B = random.choice(data)
-score = 1
 game_over = True
 
 compare = f"Compare A: {compare_A['name']}, {compare_A['description']}, {compare_A['country']}."
 against = f"Against B: {against_B['name']}, {against_B['description']}, {against_B['country']}."
+
+def game() :
+  score = 0
+  compare_a_count = compare_A['follower_count']
+  against_b_count = against_B['follower_count']
+  
+  if compare_a_count > against_b_count :
+    print(f"You're right! Current score: {score}.")
+    score += 1
+  else :
+    print(logo)
+    print(f"Sorry, that's wrong. Final score: {score}.")
+    return False
 
 while game_over :
   print(logo)
@@ -18,25 +30,9 @@ while game_over :
   question = input("Who has more followers? Type 'A' or 'B':   ").upper()
   
   if question=="A" :
-    compare_A_count = compare_A['follower_count']
-    against_B_count = against_B['follower_count']
-    
-    if compare_A_count > against_B_count :
-      print(f"You're right! Current score: {score}.")
-      score += 1
-    else :
-      print(logo)
-      print(f"Sorry, that's wrong. Final score: {score}.")
-      game_over = False
+    game()
   
   else :
-    compare_A_count = compare_A['follower_count']
-    against_B_count = against_B['follower_count']
+    game()
     
-    if compare_A_count > against_B_count :
-      print(f"You're right! Current score: {score}.")
-      score += 1
-    else :
-      print(logo)
-      print(f"Sorry, that's wrong. Final score: {score}.")
-      game_over = False
+    
