1bd84405d736b9e807698bb9eed47e058c62ad3c
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 20:45:18 +0530

    test fizzbuzz game

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Done")
+def fizz_buzz(number):
+  for number in range(1, number):
+    if number % 5 == 0 and number % 3 == 0:
+      print("FizzBuzz")
+    elif number % 5 == 0:
+      print("Buzz")
+    elif number % 3 == 0:
+      print("Fizz")
+    else:
+      print(number)
+      
+fizz_buzz(10)
