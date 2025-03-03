874957c11578f8295156f4cb69802ec107f121e6
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 21:43:32 +0530

    install flask

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
+from  flask import Flask
 
-user_input = int(input("Enter your number: "))
-
-def fizz_buzz():
-  for number in range(1, user_input):
-    if number % 5 == 0 and number % 3 == 0:
-      print("FizzBuzz")
-    elif number % 5 == 0:
-      print("Buzz")
-    elif number % 3 == 0:
-      print("Fizz")
-    else:
-      print(number)
-      
-fizz_buzz()
+print(Flask)
