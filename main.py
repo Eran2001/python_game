95079b25214ede08618514ad1912503319d37754
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 19:50:00 +0530

    change some

--- main.py
+++ main.py
@@ -40,10 +40,3 @@ while game_over :
       print(logo)
       print(f"Sorry, that's wrong. Final score: {score}.")
       game_over = False
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Eran2001/python_game.git
-git push -u origin main
