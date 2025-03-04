186055df67c3625f3b3fef96d5d6ffcfc590c37a
Eran <eran.hasarali.2001@gmail.com>
2025-03-04 12:17:45 +0530

    add turtle color

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 my_turtle = Turtle()
 my_turtle.shape("turtle")
+my_turtle.color("red", "green")
 my_turtle.forward(100)
 my_turtle.left(90)
 my_turtle.forward(100)
