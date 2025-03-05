79f44965cb9f83f95d5ec3e84e52523b8b9f74cd
Eran <eran.hasarali.2001@gmail.com>
2025-03-05 19:16:02 +0530

    get attr using bs4

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from bs4 import BeautifulSoup
 import lxml
 
+doc = """
+<form method="get" action="/search/">
+ <input type="text" name="q" maxlength="255" value=""></input>
+</form>
+"""
+
 with open("website.html") as file:
   contents = file.read()
   
 soup = BeautifulSoup(contents, "lxml")
 
-print(soup.select(selector="body a"))
+html_doc = BeautifulSoup(doc, "lxml")
+
+print(html_doc.input["maxlength"])
+
