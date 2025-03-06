d926455349f72739074780bc834a3bcd2944df53
Eran <eran.hasarali.2001@gmail.com>
2025-03-06 10:56:48 +0530

    get data from live

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import lxml
 import requests
 
 response = requests.get("https://news.ycombinator.com/news")
-print(response.text)
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "lxml")
+title = soup.find(name="span", class_="titleline")
+print(title.get_text())
+
