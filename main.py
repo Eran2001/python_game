87cf929b6feb1f7ad67d1b7bef90fd93881c9e94
Eran <eran.hasarali.2001@gmail.com>
2025-03-06 11:28:40 +0530

    get 100 movies

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from bs4 import BeautifulSoup
 import lxml
 import requests
 
-response = requests.get("https://news.ycombinator.com/news")
-yc_web_page = response.text
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 
-soup = BeautifulSoup(yc_web_page, "lxml")
-title = soup.find(name="span", class_="titleline")
-print(title.get_text())
+website = response.text
 
+soup = BeautifulSoup(website, "lxml")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+
+movie_title = [movie.getText() for movie in all_movies]
+print(movie_title[::-1])
