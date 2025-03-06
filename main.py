1da67fea35a4c1bd06de76aa32fca9f1d7d72579
Eran <eran.hasarali.2001@gmail.com>
2025-03-06 11:47:45 +0530

    get 100 songs title

---
+++ main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+URL = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12/", headers=headers)
+
+response = URL.text
+
+soup = BeautifulSoup(response, "lxml")
+
+heading = soup.find(name="h1", id="section-heading")
+
+print(heading.text.strip())
